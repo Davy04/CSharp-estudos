5ac1917d3a4750e3639c05807b50961ea0cf066d
Davy Woolley <davywoolley@gmail.com>
2023-10-18 19:36:27 -0300

    Primeira solução com Classes

---
+++ ProjetoUm/ProjetoUm/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProjetoUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("entre com as medidas do triangulo X");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("entre com as medidas do triangulo Y");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("AREA DE X: " + areaX);
+            Console.WriteLine("AREA DE Y: " + areaY);
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("O Triangulo X é maior");
+            }
+            else
+            {
+                Console.WriteLine("O Triangulo Y é maior");
+            }
+        }
+    }
+}
