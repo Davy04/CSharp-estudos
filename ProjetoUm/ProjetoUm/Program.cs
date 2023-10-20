7ffbbdc21d15d749ac8e8173fb4091b212b0bf13
Davy Woolley <davywoolley@gmail.com>
2023-10-20 19:03:38 -0300

    att

--- ProjetoUm/ProjetoUm/Program.cs
+++ ProjetoUm/ProjetoUm/Program.cs
@@ -7,36 +7,28 @@ namespace ProjetoUm
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
-            x = new Triangulo();
-            y = new Triangulo();
+            Retangulo ret = new Retangulo();
 
-            Console.WriteLine("entre com as medidas do triangulo X");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre a largura e a altura do Retangulo: ");
+            Console.Write("altura: ");
+            ret.altura = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("entre com as medidas do triangulo Y");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("largura: ");
+            ret.largura = double.Parse(Console.ReadLine());
+
+            double area = ret.CalcArea();
+
+            Console.WriteLine("AREA: " + area);
+
+            double peri = ret.CalcPeri();
+
+            Console.WriteLine("PERIMETRO: " + peri);
+
+            double diag = ret.CalcDiag();
+
+            Console.WriteLine("DIAGONAL: " + diag);
 
 
-            double areaX = x.CalcArea();
-
-            double areaY = y.CalcArea();
-
-            Console.WriteLine("AREA DE X: " + areaX);
-            Console.WriteLine("AREA DE Y: " + areaY);
-
-            if (areaX > areaY)
-            {
-                Console.WriteLine("O Triangulo X é maior");
-            }
-            else
-            {
-                Console.WriteLine("O Triangulo Y é maior");
-            }
         }
     }
 }
---
+++ ProjetoUm/ProjetoUm/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoUm
+{
+    internal class Retangulo
+    {
+        public double largura;
+        public double altura;
+
+        public double CalcArea()
+        {
+            return largura * altura;
+        }
+
+        public double CalcPeri()
+        {
+            return (largura * 2) + (altura * 2);
+        }
+
+        public double CalcDiag()
+        {
+            return Math.Sqrt((largura * largura) + (altura * altura));
+        }
+    }
+}
