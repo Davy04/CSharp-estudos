4ef398e0b562ab60237964b549d5d5e794ee0e5d
Davy Woolley <davywoolley@gmail.com>
2023-10-23 20:30:06 -0300

    Encapsulamento

--- ProjProdu/ProjProdu/Produto.cs
+++ ProjProdu/ProjProdu/Produto.cs
@@ -9,35 +9,58 @@ namespace ProjProdu
 {
     class Produto
     {
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto() { }
 
         public Produto(string nome, double preco, int quantidade)
         {
-            Nome = nome; Preco = preco; Quantidade = quantidade;
+            _nome = nome; _preco = preco; _quantidade = quantidade;
+        }
+
+        public string GetNome() { return _nome; }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
         }
 
         public double ValorTotalEmEstoque()
         {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public override string ToString()
         {
-            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) +
-                ", " + Quantidade + " Unidades, Total: $ " +
+            return _nome + ", $ " + _preco.ToString("F2", CultureInfo.InvariantCulture) +
+                ", " + _quantidade + " Unidades, Total: $ " +
                 ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
 
         public void AdicionarProduto(int quantidade)
         {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProduto(int quantidade)
         {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
     }
