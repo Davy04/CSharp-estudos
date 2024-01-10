706be9aee0ec522f6032e48bd5787913ae3018d2
Davy <davywoolley@gmail.com>
2024-01-10 17:24:08 -0300

    att

---
+++ FuncTerc/FuncTerc/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncTerc.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public int ValuePerHour {  get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, int valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
